9c690c6722521882d6e058cb1b5bec1488ea0749
shengbi-jiang <shengbi.jiang@gmail.com>
2021-08-21 21:31:18 +0800

    feat(config): set up TypeOrm module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppConfigModule } from './app-config/app-config.module';
+import { TypeormConfigModule } from './typeorm-config/typeorm-config.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, TypeormConfigModule],
 })
 export class AppModule {}

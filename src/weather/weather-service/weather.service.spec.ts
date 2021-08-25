1aae55e1977748682c6086d0e0810aee99bdbc56
shengbi-jiang <shengbi.jiang@gmail.com>
2021-08-25 11:21:07 +0800

    test(weather): create unit tests for WeatherService.updateCurrentWeather

--- src/weather/weather-service/weather.service.spec.ts
+++ src/weather/weather-service/weather.service.spec.ts
@@ -3,7 +3,10 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { DeepPartial, FindConditions } from 'typeorm';
 import { Weather } from '../weather.entity';
-import { Location as WeatherAtLocation } from '../weather-retriever-service/weather-response-result.type';
+import {
+  WeatherResponseResult,
+  Location as WeatherAtLocation,
+} from '../weather-retriever-service/weather-response-result.type';
 import { WeatherService } from './weather.service';
 
 function createWeather() {
@@ -138,6 +141,74 @@ describe('WeatherService', () => {
       expect(weather).toBe(WEATHER);
       expect(deps.WeatherRepository.create).toBeCalled();
       expect(deps.WeatherRepository.save).toBeCalled();
+    });
+  });
+
+  describe('updateCurrentWeather', () => {
+    it('should create a new weather and update an existing weather data', async () => {
+      const TAIPEI_CITY = 'Taipei';
+      const TAINAN_CITY = 'Tainan';
+
+      const TAIPEI_WEATHER_LOCATION: WeatherAtLocation = {
+        locationName: TAIPEI_CITY,
+        weatherElement: [],
+      };
+
+      const TAINAN_WEATHER_LOCATION: WeatherAtLocation = {
+        locationName: TAINAN_CITY,
+        weatherElement: [],
+      };
+
+      const TAIPEI_WEATHER = new Weather();
+      TAIPEI_WEATHER.id = 'UUID-TAIPEI';
+      TAIPEI_WEATHER.city = TAIPEI_CITY;
+      TAIPEI_WEATHER.data = {}; // assume it's old data
+
+      const TAINAN_WEATHER = new Weather();
+      TAINAN_WEATHER.id = 'UUID-TAINAN';
+      TAINAN_WEATHER.city = TAINAN_CITY;
+      TAINAN_WEATHER.data = TAINAN_WEATHER_LOCATION as any;
+
+      const input: WeatherResponseResult = {
+        locations: [TAIPEI_WEATHER_LOCATION, TAINAN_WEATHER_LOCATION],
+      };
+
+      const deps = await prepare({
+        WeatherRepository: {
+          findOne: jest.fn(async (conditions: FindConditions<Weather>) => {
+            expect(conditions).toBeDefined();
+            expect(conditions.city).toEqual(expect.any(String));
+            if (conditions.city === TAIPEI_WEATHER.city) {
+              return TAIPEI_WEATHER;
+            } else if (conditions.city === TAINAN_WEATHER.city) {
+              return null;
+            } else {
+              fail(`Out of test cases - unknown city: ${conditions.city}`);
+            }
+          }),
+          create: jest.fn((entityLike: DeepPartial<Weather>) => {
+            expect(entityLike.id).toEqual(expect.any(String));
+            expect(entityLike.city).toBe(TAINAN_WEATHER_LOCATION.locationName);
+            expect(entityLike.data).toBe(TAINAN_WEATHER_LOCATION);
+            return TAINAN_WEATHER;
+          }),
+          save: jest.fn(async (entity: Weather) => {
+            if (entity.city === TAIPEI_WEATHER.city) {
+              expect(entity.data).toBe(TAIPEI_WEATHER_LOCATION);
+            } else if (entity.city === TAINAN_WEATHER.city) {
+              expect(entity.data).toBe(TAINAN_WEATHER_LOCATION);
+            } else {
+              fail(`Out of test cases - unknown city: ${entity.city}`);
+            }
+            return entity;
+          }),
+        },
+      });
+
+      await service.updateCurrentWeather(input);
+      expect(deps.WeatherRepository.findOne).toBeCalledTimes(2);
+      expect(deps.WeatherRepository.create).toBeCalledTimes(1);
+      expect(deps.WeatherRepository.save).toBeCalledTimes(2);
     });
   });
 });

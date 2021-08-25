8359a59c9c5f3d641058fb7d89a008beba505228
shengbi-jiang <shengbi.jiang@gmail.com>
2021-08-25 09:41:11 +0800

    test(weather): create a general case and a exception case for WeatherController unit tests

--- src/weather/weather.controller.spec.ts
+++ src/weather/weather.controller.spec.ts
@@ -1,18 +1,61 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { WeatherService } from './weather-service/weather.service';
 import { WeatherController } from './weather.controller';
 
 describe('WeatherController', () => {
   let controller: WeatherController;
 
-  beforeEach(async () => {
+  async function prepare(deps: any = {}) {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [{ provide: WeatherService, useValue: deps.WeatherService ?? {} }],
       controllers: [WeatherController],
     }).compile();
 
     controller = module.get<WeatherController>(WeatherController);
+    return deps;
+  }
+
+  it('should be defined', async () => {
+    await prepare();
+    expect(controller).toBeDefined();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return the weather data of a specified city', async () => {
+    const CITY_NAME = 'Taipei';
+    const WEATHER = { data: 'SAMPLE' };
+    const deps = await prepare({
+      WeatherService: {
+        mustReadOneByCity: jest.fn(async (city: string) => {
+          expect(city).toBe(CITY_NAME);
+          return WEATHER;
+        }),
+      },
+    });
+
+    const data = await controller.getWeather(CITY_NAME);
+    expect(data).toBe(WEATHER.data);
+    expect(deps.WeatherService.mustReadOneByCity).toBeCalledWith(CITY_NAME);
+  });
+
+  it('should not catch any exception', async () => {
+    const CITY_NAME = 'XYZ';
+    const notFoundException = new NotFoundException();
+    const deps = await prepare({
+      WeatherService: {
+        mustReadOneByCity: jest.fn(async (city: string) => {
+          expect(city).toBe(CITY_NAME);
+          throw notFoundException;
+        }),
+      },
+    });
+
+    try {
+      await controller.getWeather(CITY_NAME);
+      fail('The controller should not catch any exception');
+    } catch (err) {
+      expect(err).toBe(notFoundException);
+      expect(deps.WeatherService.mustReadOneByCity).toBeCalledWith(CITY_NAME);
+    }
   });
 });

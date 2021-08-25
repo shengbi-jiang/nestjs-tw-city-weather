e206772fbecd186269f8aaf655e88d1568f7e922
shengbi-jiang <shengbi.jiang@gmail.com>
2021-08-25 10:09:56 +0800

    test(weather): create unit tests for WeatherService.readOneByCity

--- src/weather/weather-service/weather.service.spec.ts
+++ src/weather/weather-service/weather.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { FindConditions } from 'typeorm';
+import { Weather } from '../weather.entity';
 import { WeatherService } from './weather.service';
+
+function createWeather() {
+  const weather = new Weather();
+  weather.id = 'UUID';
+  weather.city = 'Taipei';
+  weather.data = {};
+  return weather;
+}
 
 describe('WeatherService', () => {
   let service: WeatherService;
 
-  beforeEach(async () => {
+  async function prepare(deps: any = {}) {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [WeatherService],
+      providers: [
+        WeatherService,
+        {
+          provide: getRepositoryToken(Weather),
+          useValue: deps.WeatherRepository ?? {},
+        },
+      ],
     }).compile();
 
     service = module.get<WeatherService>(WeatherService);
+    return deps;
+  }
+
+  it('should be defined', async () => {
+    await prepare();
+    expect(service).toBeDefined();
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('readOneByCity', () => {
+    it('should return weather data when the weather data of a specified city is found', async () => {
+      const WEATHER = createWeather();
+      const CITY_NAME = WEATHER.city;
+      const deps = await prepare({
+        WeatherRepository: {
+          findOne: jest.fn(async (conditions: FindConditions<Weather>) => {
+            expect(conditions).toEqual({ city: CITY_NAME });
+            return WEATHER;
+          }),
+        },
+      });
+
+      const weather = await service.readOneByCity(CITY_NAME);
+      expect(weather).toBe(WEATHER);
+      expect(deps.WeatherRepository.findOne).toBeCalled();
+    });
+
+    it('should return null when the weather data of a specified city is not found', async () => {
+      const CITY_NAME = 'Tainan';
+      const deps = await prepare({
+        WeatherRepository: {
+          findOne: jest.fn(async (conditions: FindConditions<Weather>) => {
+            expect(conditions).toEqual({ city: CITY_NAME });
+            return null;
+          }),
+        },
+      });
+
+      const weather = await service.readOneByCity(CITY_NAME);
+      expect(weather).toBeNull();
+      expect(deps.WeatherRepository.findOne).toBeCalled();
+    });
   });
 });
